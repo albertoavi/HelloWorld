9a4e00fc1cd52e0673c60ab287be738eee3f64f8
Alberto Avi <alberto.avi@gmail.com>
2016-06-30 16:23:38 +0200

    Some minor changes

--- src/it/bigip/helloworld/HelloWorld.java
+++ src/it/bigip/helloworld/HelloWorld.java
@@ -22,7 +22,7 @@ public class HelloWorld {
     }
 
     public static void main(String [] args){
-        HelloWorld hello = new HelloWorld();
-        hello.printSysInfo();
+        HelloWorld main = new HelloWorld();
+        main.printSysInfo();
     }
 }
