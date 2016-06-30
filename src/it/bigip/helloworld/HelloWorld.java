e44e95451554eb75ca277438008ed755554585c2
Alberto Avi <alberto.avi@gmail.com>
2016-06-30 15:54:21 +0200

    Some minor changes

--- src/it/bigip/helloworld/HelloWorld.java
+++ src/it/bigip/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package it.bigip.helloworld;
 
+import java.util.Map;
+
 /**
  * Created by alberto on 6/30/16.
  */
@@ -8,15 +10,18 @@ public class HelloWorld {
     public HelloWorld() {
     }
 
-    private void doSomething(){
-        System.out.println(System.currentTimeMillis() + " Hello world!");
+    private void printSysInfo(){
+        // this function is develeped to test dev-branch
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+            System.out.format("%s=%s%n",
+                    envName,
+                    env.get(envName));
+        }
     }
 
     public static void main(String [] args){
         HelloWorld hello = new HelloWorld();
-
-        for (int x=0; x<20; x++) {
-            hello.doSomething();
-        }
+        hello.printSysInfo();
     }
 }
